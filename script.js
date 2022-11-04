6f9763191ed9236e1eb9257103e58176d78f0427
AdsonVilla <adson23_@hotmail.com>
2022-11-04 16:21:19 -0300

    commit de alterações no layout e responsividade

---
+++ script.js
@@ -0,0 +1,12 @@
+ScrollReveal({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+}).reveal(`
+  #home,
+  #home img,
+  #advantages .advantage,
+  #services,
+  #gabriela-section,
+  #doubts,
+  #contact`);
